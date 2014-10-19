cbb988f45bf59b292d702b2b96e2c009eff45e62
James Menera <menerajames@gmail.com>
2014-10-18 19:14:29 -0700

    check in code before trying experimental ember build

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,15 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-var app = new EmberApp();
+var app = new EmberApp({
+  env: 'development',
+  sassOptions: {
+    inputFile: 'ember-steroids.sass',
+    outputFile: 'ember-steroids.css',
+    includePaths: ['app/styles', 'node_modules/bourbon-libsass/dist'],
+    sourceMap: true
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
